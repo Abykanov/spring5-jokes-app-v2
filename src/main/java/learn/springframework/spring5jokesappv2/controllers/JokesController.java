cd42e6cad02c2a0bcec42b983d94b3be44b0cafc
Zhanysh Orozobakov <orozobakov96@gmail.com>
2022-09-13 14:36:36 +0600

    Add Joke Controller and index.html

---
+++ src/main/java/learn/springframework/spring5jokesappv2/controllers/JokesController.java
@@ -0,0 +1,24 @@
+package learn.springframework.spring5jokesappv2.controllers;
+
+import learn.springframework.spring5jokesappv2.services.JokeService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class JokesController {
+    private final JokeService jokeService;
+
+    @Autowired
+    public JokesController(JokeService jokeService) {
+        this.jokeService = jokeService;
+    }
+
+    @RequestMapping({"/", ""})
+    public String showJoke(Model model) {
+        model.addAttribute("joke", jokeService.getJoke());
+
+        return "index";
+    }
+}
